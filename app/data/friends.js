426dcafc3e30f362ba5bf1767eb05f04eb4cfc7f
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-06-20 21:00:47 -0700

    Adding mock data

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var friendsArray = [
+    {
+        "name":"Becky Lucas",
+        "photo":"https://randomuser.me/api/portraits/women/11.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Max Bryant",
+        "photo":"https://randomuser.me/api/portraits/men/67.jpg",
+        "scores":[
+            3,
+            4,
+            5,
+            3,
+            1,
+            2,
+            3,
+            5,
+            3,
+            2
+        ]
+    },
+    {
+        "name":"Curtis Mendoza",
+        "photo":"https://randomuser.me/api/portraits/men/64.jpg",
+        "scores":[
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            5,
+            4,
+            4,
+            1
+        ]
+    }
+  ];
+  
+  module.exports = friendsArray;
+  
