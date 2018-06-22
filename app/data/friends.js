d8224e36670a65d84e2cbbaa6513e7fc3259c2be
Anthony Penalosa <aj@Anthonys-MacBook-Pro.local>
2018-06-21 19:51:11 -0700

    Adding more friends to the database

--- app/data/friends.js
+++ app/data/friends.js
@@ -50,6 +50,118 @@ var friendsArray = [
             4,
             1
         ]
+    },
+    {
+        "name":"Ben Adams",
+        "photo":"https://randomuser.me/api/portraits/men/10.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            2,
+            2,
+            3,
+            1,
+            2,
+            5,
+            1
+        ]
+    },
+    {
+        "name":"Katrina Garcia",
+        "photo":"https://randomuser.me/api/portraits/women/29.jpg",
+        "scores":[
+            5,
+            4,
+            5,
+            3,
+            5,
+            4,
+            3,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name":"Lawrence Baker",
+        "photo":"https://randomuser.me/api/portraits/men/32.jpg",
+        "scores":[
+            5,
+            3,
+            4,
+            4,
+            2,
+            1,
+            5,
+            4,
+            5,
+            3
+        ]
+    },
+    {
+        "name":"Kaylee May",
+        "photo":"https://randomuser.me/api/portraits/women/52.jpg",
+        "scores":[
+            3,
+            4,
+            5,
+            4,
+            4,
+            5,
+            3,
+            5,
+            5,
+            4
+        ]
+    },
+    {
+        "name":"Katherine Fletcher",
+        "photo":"https://randomuser.me/api/portraits/women/92.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name":"Allen Duncan",
+        "photo":"https://randomuser.me/api/portraits/men/90.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Julian Daniels",
+        "photo":"https://randomuser.me/api/portraits/men/77.jpg",
+        "scores":[
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5
+        ]
     }
   ];
   
